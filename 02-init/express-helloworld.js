dac120c82b27b38931b35dc5319f9111eaa48a24
10488 <1048878258@qq.com>
2024-03-19 20:54:06 +0800

    Q&A 1.1, 删除了冗余文件，添加本地保存用户到users.json中

--- 02-init/express-helloworld.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express');	//	版本4.17.1
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => res.send('Hello World!'));
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- 02-init/hello-api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = new express();
-
-app.use(express.static('web'));
-
-app.listen(80,() => {
-    console.log('server listening on port:80');
-});
